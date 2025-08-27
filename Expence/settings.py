eb6c6c3f0968cd0b1c49aa89921079adaf019838
Sujal Sawalkar <sujalsawalkar2005@gmail.com>
2025-08-27 22:54:47 +0530

    Added deployment configs

--- Expence/settings.py
+++ Expence/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uyjdth*@o+qv7j$bz18%*_ux!gv-w3pp@^uxd%-r)hv*pvcl%y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
