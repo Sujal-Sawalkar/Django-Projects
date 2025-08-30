0d18dae18d62cc37bfb0a62876efda1278f56876
Sujal Sawalkar <sujalsawalkar2005@gmail.com>
2025-08-31 02:33:53 +0530

    Prepare for Railway deployment with psycopg3 and Procfile

--- Expence/settings.py
+++ Expence/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uyjdth*@o+qv7j$bz18%*_ux!gv-w3pp@^uxd%-r)hv*pvcl%y'
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-for-dev")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -106,16 +107,23 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "ET",
+            "USER": "sujal",
+            "PASSWORD": "Sujal@2005",
+            "HOST": "localhost",
+            "PORT": "5432",
         }
     }
-# 3. Procfile for Railway
 # if os.environ.get("RENDER"):  # When running on Render
 #     DATABASES = {
 #        "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
@@ -176,3 +184,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
