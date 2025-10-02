16d8261d1886feb2a0da5d9577f9efcce06c5cad
Sujal-Sawalkar <sujalsawalkar2005@gmail.com>
2025-10-02 21:00:33 +0530

    Update urls.py

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login, name='login'), 
     path('logout/', views.logout, name='logout'),
     path('add_expense/', views.add_expense, name='add_expense'),
     path('view_expenses/', views.view_expenses, name='view_expenses'),
